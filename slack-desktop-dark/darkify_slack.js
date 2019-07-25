80eaeed868115eee9b7351872eda8dd6f26c639f
yochananmarqos <yochanan.marqos@gmail.com>
2019-07-25 14:38:06 -0600

    added slack-desktop & slack-desktop-dark

---
+++ slack-desktop-dark/darkify_slack.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function() {
+    let s = document.createElement('style');
+    s.type = 'text/css';
+    s.innerHTML = `
+HERE
+`;
+    document.head.appendChild(s);
+});
