8d01dfe7bccd950de4ce31ef8cd980fbae47be6e
yochananmarqos <yochanan.marqos@gmail.com>
2019-09-13 08:18:13 -0600

    removed slack-desktop & slack-desktop-dark

--- slack-desktop-dark/darkify_slack.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    let s = document.createElement('style');
-    s.type = 'text/css';
-    s.innerHTML = `
-HERE
-`;
-    document.head.appendChild(s);
-});
