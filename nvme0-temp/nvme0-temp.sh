bcc2ca86644e0709f3bedb5aeddb7941c944f37f
yochananmarqos <yochanan.marqos@gmail.com>
2019-10-29 19:42:31 -0600

    updated nvme0-temp

--- nvme0-temp/nvme0-temp.sh
+++ nvme0-temp/nvme0-temp.sh
@@ -5,7 +5,7 @@ tempfile="/tmp/nvme0-temp"
 trap "rm -f "$tempfile"; exit" 0 1 2 3 13 15  # Exit, HUP, INT, QUIT, PIPE, TERM
 
 while true; do
-    temperature="$(/usr/bin/nvme smart-log /dev/nvme0 | /usr/bin/sed -n '/^[Tt]emperature *: *\([0-9]*\).*/{s//\1/p;q}')"
+    temperature="$(nvme smart-log /dev/nvme0 | sed -n '/^[Tt]emperature *: *\([0-9]*\).*/{s//\1/p;q}')"
     builtin echo "$temperature" > "$tempfile"
     builtin sleep 2
 done
