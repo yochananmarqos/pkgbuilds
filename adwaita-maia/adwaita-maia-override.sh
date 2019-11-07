ac81ab4a9017736b869f312cbff6a3448fe0e882
yochananmarqos <yochanan.marqos@gmail.com>
2019-11-07 08:30:33 -0700

    updated adwaita-maia

--- adwaita-maia/adwaita-maia-override.sh
+++ adwaita-maia/adwaita-maia-override.sh
@@ -3,6 +3,11 @@ cmd=$1
 file="${@}"
 gtk_theme=$(dconf read /org/gnome/desktop/interface/gtk-theme | tr -d "'")
 
+if [ -z $1 ]; then
+	echo "No command given"
+	exit 0
+fi
+
 if [ $gtk_theme != Adwaita ]; then
 	if [ -z "$file" ]; then
 		GTK_THEME="${gtk_theme}" exec "$cmd"
