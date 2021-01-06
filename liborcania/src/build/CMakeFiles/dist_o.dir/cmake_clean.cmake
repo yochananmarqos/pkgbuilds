f19b42368a893c5bd4e5ac50e01f597f2baa63d0
Mark Wagie <mark.wagie@tutanota.com>
2021-01-06 14:33:21 -0700

    removed files

--- liborcania/src/build/CMakeFiles/dist_o.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/dist_o"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/dist_o.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
