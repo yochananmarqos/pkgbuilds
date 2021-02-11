c6cf411786e6f0eda5439ff3a4e9813444afe116
Mark Wagie <mark.wagie@tutanota.com>
2021-02-11 13:49:36 -0700

    updated keyboard-color-switcher-git

--- keyboard-color-switcher-git/keyboard-color-switcher.sh
+++ keyboard-color-switcher-git/keyboard-color-switcher.sh
@@ -2,4 +2,4 @@
 
 _site_packages=$(python -c "import site; print(site.getsitepackages()[0])")
 
-pkexec "$_site_packages"/keyboard-color-switcher/keyboard-color-switcher.py
+gksu "$_site_packages"/keyboard-color-switcher/keyboard-color-switcher.py
