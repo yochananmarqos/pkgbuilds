7a06d32db9358e01260adfbaea4fe08ce0d9566c
Mark Wagie <mark.wagie@proton.me>
2023-05-28 08:45:07 -0600

    removed savedesktop

--- savedesktop/savedesktop.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/bash
-if [ "$1" == "" ]
-then 
-  python /usr/share/savedesktop/main_window.py
-elif [ "$1" == "--background" ]
-  then
-    echo "Periodic saving is currently only supported in Flatpak."
-fi
