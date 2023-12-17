e5f525fbd4c7be063ba9c63dd3432978a97f7f21
Mark Wagie <mark.wagie@proton.me>
2023-12-17 11:33:27 -0700

    updated savedesktop

--- savedesktop/savedesktop.sh
+++ savedesktop/savedesktop.sh
@@ -2,10 +2,19 @@
 cd /usr/share/savedesktop
 if [ "$1" == "" ]
 then 
-  python /usr/share/savedesktop/main_window.py
+  python main_window.py
 elif [ "$1" == "--background" ]
   then
-    echo "Periodic saving is currently only supported in Flatpak."
+    python periodic_saving.py
+elif [ "$1" == "--sync" ]
+  then
+    python network_sharing.py
+elif [ "$1" == "--start-server" ]
+  then
+    python server.py
+elif [ "$1" == "--help" ]
+  then
+       echo -e '\033[1mArguments:\033[0m \n None | Run SaveDesktop app (GUI) \n --background | Start periodic saving \n --sync | Sync desktop configuration with other computer \n --start-server | Start HTTP server for synchronization DE config with other computers \n --help | Show this message'
 fi
 
 cd
